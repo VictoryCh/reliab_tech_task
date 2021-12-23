f22db9d31a0c1eb57a31659573b387f8ed6067b4
Viсtoria_Chernenko <VChernenko@bellintegrator.com>
2021-12-23 17:13:39 +0300

    init

---
+++ src/main/java/ru/chernenko/interview/reliabtech/ReliabTechTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.chernenko.interview.reliabtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliabTechTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReliabTechTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/chernenko/interview/reliabtech/ReliabTechTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chernenko.interview.reliabtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliabTechTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
